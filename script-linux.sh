38cb1948d6eab5c09a51df9699873cc8d55a0f29
Sohil876 <sohil876@gmail.com>
2022-02-22 16:28:19 +0530

    Clean repo, setup linux workflow

--- script-linux.sh
+++ script-linux.sh
@@ -1,1 +1,15 @@
-echo Edit script-linux.sh in your fastmac repo to auto-run commands in your linux instances
+#!/bin/bash
+#
+## Config
+#
+
+#Git
+git config --global pull.rebase false
+git config --global user.name "Sohil876"
+git config --global user.email "${EMAIL}"
+
+# Apt
+#sudo apt update && sudo apt upgrade -y
+sudo apt install micro
+sudo apt clean && sudo apt autoclean
+
