8a2d4f28199d2e6ce0b80fe6cc4a0270edea0004
Sohil876 <sohil876@gmail.com>
2022-06-15 02:42:37 +0530

    Use env var for github username, install rclone by default

--- script-linux.sh
+++ script-linux.sh
@@ -5,11 +5,14 @@
 
 # Git
 #git config --global pull.rebase false
-git config --global user.name "Sohil876"
+git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${EMAIL}"
 
 # Apt
 #sudo apt update && sudo apt upgrade -y
-sudo apt install micro
+sudo apt install micro rclone
 sudo apt clean && sudo apt autoclean
 
+# Misc
+mkdir -p ~/.config/rclone/
+echo "${RCLNCNF}" > ~/.config/rclone/rclone.conf
